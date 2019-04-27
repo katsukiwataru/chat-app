d13e0e7de3f5bb6974b1043324258fd0da269378
katsukiwataru <a17dc050@dhw.ac.jp>
2019-04-27 19:01:14 +0900

    first commit

---
+++ src/nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  buildDir: '../functions/nuxt',
+  head: {
+    title: 'src',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#3B8070' },
+  build: {
+    publicPath: '/assets/',
+    extractCSS: true,
+    babel: {
+      presets: [
+        'env',
+        'stage-0'
+      ],
+      plugins: [
+        ['transform-runtime', {
+          polyfill: true,
+          regenerator: true
+        }],
+      ],
+    },
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
