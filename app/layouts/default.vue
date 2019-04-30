3e5cade2baa16fb176140d67392f3c1993c7e3d6
katsukiwataru <a17dc050@dhw.ac.jp>
2019-05-01 01:46:52 +0900

    add auth firebase

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
-.wrapper
-  nuxt.container
+  .wrapper
+    nuxt.container
 </template>
 
 <script>
 </script>
+
+<style lang="scss">
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,69 @@
 <template lang="pug">
-div
-  TheHeader
+.inner
+  .header
+    h1 message app
+    .isLogin(
+      v-if="!isLogin"
+    )
+      button.LoginButton(@click="twitterLogin") Twitterでログインする
+    .isLogout(v-else)
+      p.userName {{ user.displayName }}
+      button.LogoutButton(@click="logout") ログアウト
 </template>
 
 <script>
-import TheHeader from '~/components/header'
+import firebase from '~/plugins/firebase'
+import "firebase/auth"
+
 export default {
   components: {
-    TheHeader
+  },
+  asyncData () {
+    return {
+      isLogin: false,
+      user: []
+    }
+  },
+  beforeMount () {
+    firebase.auth().onAuthStateChanged(user =>{
+      if (user) {
+        this.isLogin = true
+        this.user = user
+      } else {
+        this.isLogin = false
+        this.user = []
+      }
+      console.log(this.isLogin, this.user)
+    })
+  },
+  methods: {
+    twitterLogin () {
+      const provider = new firebase.auth.TwitterAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+    },
+    logout () {
+      firebase.auth().signOut()
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.inner {
+  & button {
+    border: none;
+    font-size: 16px;
+  }
+  & .header {
+    margin: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  & .LoginButton {
+  }
+  & .isLogout {
+    display: flex;
+  }
+}
 </style>
