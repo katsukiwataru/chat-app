27a30b1de3ebbd1cbcf9a938136ea13b30faf99c
katsukiwataru <a17dc050@dhw.ac.jp>
2019-05-03 21:40:53 +0900

    add vuex

--- app/store/index.js
+++ app/store/index.js
@@ -1,27 +1,47 @@
-// export const state = () => ({
-//   user: null,
-//   isLoggedIn: false,
-//   messages: []
-// })
+import firebase from '~/plugins/firebase.js'
 
-// export const getters = {
-//   user: state => state.user,
-//   isLoggedIn: state => state.isLoggedIn,
-//   getMessages: state => state.messages
-// }
+export const state = () => ({
+  user: null,
+  isLoggedIn: false,
+  messages: []
+})
 
-// export const mutations = {
-//   setUser(state, { user }) {
-//     state.user = user
-//     state.isLoggedIn = true
-//   },
-//   updateUser(state, { user }) {
-//     state.user = user
-//   }
-// }
+export const getters = {
+  user: state => state.user,
+  isLoggedIn: state => state.isLoggedIn,
+  getMessages: state => state.messages
+}
 
-// export const actions = {
-//   register({ commit }) {
-//     commit('setUser', { user })
-//   }
-// }
+export const mutations = {
+  setUser(state, { user }) {
+    state.user = user
+    state.isLoggedIn = true
+  },
+  updateUser(state, { user }) {
+    state.user = user
+  },
+  deleteUser() {
+    state.user = null
+    state.isLoggedIn = false
+  }
+}
+
+export const actions = {
+  twitterSignIn({dispatch}) {
+    firebase.auth().signInWithRedirect(new firebase.auth.TwitterAuthProvider())
+    dispatch('twitterAuthStateChanged')
+  },
+  twittersignOut({ dispatch }) {
+    firebase.auth().signOut()
+    dispatch('twitterAuthStateChanged')
+  },
+  twitterAuthStateChanged({ dispatch, commit }) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        commit('setUser', user)
+      } else {
+        commit('deleteUser')
+      }
+    })
+  }
+}
