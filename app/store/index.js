f2a292edc71c152b62977c35a3bb0b534a1bbaec
katsukiwataru <a17dc050@dhw.ac.jp>
2019-05-04 03:00:30 +0900

    clean message

--- app/store/index.js
+++ app/store/index.js
@@ -3,18 +3,16 @@ import firebase from '~/plugins/firebase.js'
 export const state = () => ({
   user: null,
   isLoggedIn: false,
-  messages: []
 })
 
 export const getters = {
   user: state => state.user,
   isLoggedIn: state => state.isLoggedIn,
-  getMessages: state => state.messages
 }
 
 export const mutations = {
-  setUser(state, { user }) {
-    state.user = user
+  setUser(state, { account }) {
+    state.user = account
     state.isLoggedIn = true
   },
   updateUser(state, { user }) {
@@ -23,15 +21,12 @@ export const mutations = {
   deleteUser() {
     state.user = null
     state.isLoggedIn = false
-  },
-  updataMessage(state, messages) {
-    state.messages = messages
   }
 }
 
 export const actions = {
   twitterSignIn({dispatch}) {
-    firebase.auth().signInWithRedirect(new firebase.auth.TwitterAuthProvider())
+    firebase.auth().signInWithPopup(new firebase.auth.TwitterAuthProvider())
     dispatch('twitterAuthStateChanged')
   },
   twittersignOut({ dispatch }) {
@@ -39,19 +34,12 @@ export const actions = {
     dispatch('twitterAuthStateChanged')
   },
   twitterAuthStateChanged({ commit }) {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        commit('setUser', user)
+    firebase.auth().onAuthStateChanged(account => {
+      if (account) {
+        commit('setUser', account)
       } else {
         commit('deleteUser')
       }
     })
   },
-  fetchMessagesData({ commit }) {
-    const uid = firebase.auth().currentUser.uid
-    const reference = firebase.database().ref(`user/${uid}`)
-    reference.on('value', (snapshot) => {
-      commit('updataMessage', snapshot.val())
-    })
-  }
 }
