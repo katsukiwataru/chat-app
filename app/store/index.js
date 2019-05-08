cbcb411300c7a9af6359b79aa78581a26c1c3822
katsukiwataru <a17dc050@dhw.ac.jp>
2019-05-08 18:31:58 +0900

    add store

--- app/store/index.js
+++ app/store/index.js
@@ -1,45 +1,80 @@
-import firebase from '~/plugins/firebase.js'
+export const strict = false
+import firebase from '~/plugins/firebase'
 
 export const state = () => ({
   user: null,
-  isLoggedIn: false,
 })
 
 export const getters = {
-  user: state => state.user,
-  isLoggedIn: state => state.isLoggedIn,
+  isAuthenticated(state) {
+    return !!state.user
+  }
 }
 
 export const mutations = {
-  setUser(state, { account }) {
-    state.user = account
-    state.isLoggedIn = true
-  },
-  updateUser(state, { user }) {
-    state.user = user
-  },
-  deleteUser() {
-    state.user = null
-    state.isLoggedIn = false
+  setUser (state, payload) {
+    state.user = payload
   }
 }
 
 export const actions = {
-  twitterSignIn({dispatch}) {
+  twitterSignIn({ dispatch }) {
     firebase.auth().signInWithPopup(new firebase.auth.TwitterAuthProvider())
-    dispatch('twitterAuthStateChanged')
+    dispatch('test')
   },
-  twittersignOut({ dispatch }) {
-    firebase.auth().signOut()
-    dispatch('twitterAuthStateChanged')
-  },
-  twitterAuthStateChanged({ commit }) {
+  test({ dispatch }) {
     firebase.auth().onAuthStateChanged(account => {
-      if (account) {
-        commit('setUser', account)
-      } else {
-        commit('deleteUser')
-      }
+      // this.$store.dispatch('twitterSignIn')
+      dispatch('hoge', account)
     })
   },
+  hoge ({commit}, payload) {
+    commit('setUser', payload)
+  }
 }
+
+// import firebase from '~/plugins/firebase.js'
+
+// export const state = () => ({
+//   user: null,
+//   isLoggedIn: false,
+// })
+
+// export const getters = {
+//   user: state => state.user,
+//   isLoggedIn: state => state.isLoggedIn,
+// }
+
+// export const mutations = {
+//   setUser(state, { account }) {
+//     state.user = account
+//     state.isLoggedIn = true
+//   },
+//   updateUser(state, { user }) {
+//     state.user = user
+//   },
+//   deleteUser() {
+//     state.user = null
+//     state.isLoggedIn = false
+//   }
+// }
+
+// export const actions = {
+//   twitterSignIn({ dispatch }) {
+//     firebase.auth().signInWithPopup(new firebase.auth.TwitterAuthProvider())
+//     dispatch('twitterAuthStateChanged')
+//   },
+//   twittersignOut({ dispatch }) {
+//     firebase.auth().signOut()
+//     dispatch('twitterAuthStateChanged')
+//   },
+//   twitterAuthStateChanged({ commit }) {
+//     firebase.auth().onAuthStateChanged(account => {
+//       if (account) {
+//         commit('setUser', account)
+//       } else {
+//         commit('deleteUser')
+//       }
+//     })
+//   },
+// }
